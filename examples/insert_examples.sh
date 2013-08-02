6aca975445de5ebbfee3d9138fa82ff1487767f2
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-08-02 16:37:46 -0400

    Correct order

--- examples/insert_examples.sh
+++ examples/insert_examples.sh
@@ -2,5 +2,5 @@
 
 # Run all example SQL scripts to insert test data
 
-psql -U postgres -d diskprints -f slice.sql
 psql -U postgres -d diskprints -f storage.sql
+psql -U postgres -d diskprints -f slice.sql
