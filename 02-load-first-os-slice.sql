947379641d22898224350973c6804cdce38126b4
Mary Laamanen <mary.laamanen@nist.gov>
2013-10-29 16:07:32 -0400

    Include new baseline data

--- 02-load-first-os-slice.sql
+++ 02-load-first-os-slice.sql
@@ -16,8 +16,10 @@ INSERT INTO diskprint.virtualmachine (vmid, vmappname, vmappversion) VALUES ('1'
 
 -- INSERT INTO os (etid, ossystemname) VALUES ('11331-2', 'Windows XP Professional');
 INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('234-1', 'Windows XP Professional', '1', 'February 5, 2013 3:40:56PM');
+INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('11331-2', 'Windows XP Professional', '1', 'June 26, 2013 9:10:56AM');
 INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('9480-1', 'Windows 7 Ultimate 32bit', '1', 'March 3, 2012 2:30:56PM');
 INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('9480-2', 'Windows 7 Ultimate 64bit', '1', 'January 30, 2013 1:23:56PM');
+INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('9544-1', 'Windows 7 Ultimate 64bit', '1', 'July 07, 2013 10:43:30AM');
 INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('8504-1', 'Windows Vista Ultimate w/SP1 32bit', '1', 'February 12, 2012 12:20:56PM');
 INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('8504-2', 'Windows Vista Ultimate w/SP1 64bit', '1', 'May 20, 2012 3:40:56PM');
 
@@ -25,20 +27,24 @@ INSERT INTO diskprint.os (osetid, ossystemname, vmid, installdate) VALUES ('8504
 
 -- start of the modified vmsetting
 INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu, printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype,disksize, preallocatediskspace, split2gbfile, display, processor, processorram) 
-VALUES  ('1', '234-1', 'nsrl-name', 'nsrl-org', 'Disabled','Enabled', 'Disabled','Disabled', 'hard disk scsi disk','leave unchanged', 'Connected', 'Enabled','No', 'Bridged', 'Yes','NA', 'SCSI','20GB','Yes', 'No','3D Enabled', '1 cpu', '2048MB');
+VALUES  ('1', '11331-2', '11331-2', 'nsrl-org', 'Disabled','Enabled', 'Disabled','Disabled', 'hard disk IDE disk','leave unchanged', 'Connected', 'Enabled','No', 'Bridged', 'Yes','NA', 'IDE','20GB','Yes', 'No','3D Enabled', '1 cpu', '2048MB');
+
 
 INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu,  printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype, disksize, preallocatediskspace, split2gbfile, display, processor, processorram) 
-VALUES  ('1', '9480-1','Nist Nsrl', 'NIST', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk SCSI Disk', 'leave unchanged', 'Connected', 'Enabled', 'No', 'NAT', 'Yes', 'NA','IDE Disk','10GB','Yes', 'No', '3D Enabled', '1 cpu', '1024MB');
+VALUES  ('1', '9480-1','9480-1', 'NIST', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk SCSI Disk', 'leave unchanged', 'Connected', 'Enabled', 'No', 'NAT', 'Yes', 'NA','IDE Disk','10GB','Yes', 'No', '3D Enabled', '1 cpu', '1024MB');
 
  INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu,  printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype, disksize, preallocatediskspace, split2gbfile, display, processor, processorram) 
 VALUES  ('1', '9480-2','9480-2', 'NIST', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk SCSI Disk', 'leave unchanged', 'Connected', 'Enabled', 'No', 'Bridged', 'Yes', 'NA','SCSI Disk','20GB','Yes', 'No', '3D Enabled', '1 cpu', '2048MB');
 
+INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu,  printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype, disksize, preallocatediskspace, split2gbfile, display, processor, processorram) 
+VALUES  ('1', '9544-1','9544-1', 'nsrl-org', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk SCSI Disk', 'leave unchanged', 'Connected', 'Enabled', 'No', 'Bridged', 'Yes', 'NA','SCSI Disk','20GB','Yes', 'No', '3D Enabled', '1 cpu', '2048MB');
+
 INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu,  printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype, disksize, preallocatediskspace, split2gbfile, display, processor, processorram)  
-VALUES  ('1', '8504-1', 'UNKNOWN', 'NIST','Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk IDE Disk', 'leave unchanged',
+VALUES  ('1', '8504-1', '8504-1', 'NIST','Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk IDE Disk', 'leave unchanged',
 'connected', 'Enabled', 'No', 'NAT','Yes','NA','IDE Disk','8GB','Yes', 'No','3D Enabled','1 cpu', '1GB');
 
 INSERT INTO diskprint.vmsetting (vmid, osetid, vminstancename, organization, sharing, applicationmenu,  printers, autoprotect, startupdisk, advancedother, sound, usbdevice, otherdevicesconnected, network, cddvdconnected, cddvdusediskimage, harddisktype, disksize, preallocatediskspace, split2gbfile, display, processor, processorram)
-VALUES  ('1', '8504-2', 'UNKNOWN', 'NIST', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk IDE Disk', 'leave unchanged',
+VALUES  ('1', '8504-2', '8504-2', 'NIST', 'Disabled','Enabled', 'Disabled','Disabled', 'Hard Disk IDE Disk', 'leave unchanged',
 'Yes', 'Enabled', 'No', 'NAT', 'Yes','NA','IDE Disk','8GB 10GB 12GB 15GB 18GB','Yes', 'No', '3D Enabled', '1 cpu', '1GB');
 
 -- INSERT INTO slice (virtualmachineid, sliceid, slicetype, etid, applicationetid, creationdate, slicenotes, slicestate, slicepredecessorid) VALUES (1, 1, 'InitialInstall', '11331-2', '11331-2', '2012-03-30 11:11:04.155792', 'Suspended to NAS', 'latest', NULL);
